6c3b3a54ae39f5f0cf8af234e00a1aa34bd7fb9c
wailunho <arlonghk@gmail.com>
2013-09-10 17:29:09 -0700

    updated description

---
+++ Matchismo2/Supporting Files/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo2
+//
+//  Created by William Ho on 8/31/13.
+//  Copyright (c) 2013 William Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardgameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardgameAppDelegate class]));
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 matchismo2
 ==========
-cs193p assignment 3 ...
+cs193p assignment 3
+
+Set Game Rule
+=============
+
+Score
+-----
++12 for a match
+-6 for a mismatch
+-1 for selecting a card
+-16 for adding three cards when a match existed
+
+Hint
+----
+5 hints per game
+Only pressing the hint button counts as using a hint.
+
+
